3919b3cf95ae402e112a746cafc5a1066cfd0c3d
CodeSeaX <lpldiagnostic@gmail.com>
2026-09-26 17:47:33 +0530

    g

--- sw.js
+++ sw.js
@@ -1,11 +1,51 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "brushstrokes-cache-v1";
+
+// Saari files jo offline chahiye
+const urlsToCache = [
+  "./",
+  "index.html",
+  "brushstroke.css",
+  "brushstroke.js",
+  "manifest.json",
+  "brushstroke.icon.jpeg"
+];
+
+// Install: files cache mein daalo
+self.addEventListener("install", (event) => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("activate", () => {
+// Activate: purane cache versions delete karo
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
   self.clients.claim();
 });
 
+// Fetch: pehle cache se try karo, na mile to internet se lao
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        // agar dono fail ho (offline + cache mein nahi) to kuch nahi hoga
+      });
+    })
+  );
 });
